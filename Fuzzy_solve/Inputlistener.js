9a1f78c570d8b14bc63b5c3476fe91d376ef3d18
fuzzysystem <FuzzySystems@outlook.com>
2022-08-31 14:50:34 +0800

    update

---
+++ Fuzzy_solve/Inputlistener.js
@@ -0,0 +1,251 @@
+function listener_1_l(){
+    let rtn = 100;
+    let val_1_l = Number($("#F2_l_y1").val());
+    let val_cpr = Number($("#F2_u_y1").val());
+
+    if (val_1_l <= val_cpr){
+        rtn = val_1_l;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_1_u(){
+    let rtn = 100;
+    let val_1_u = Number($("#F2_u_y1").val());
+    let val_cpr = Number($("#F2_l_y1").val());
+
+    if (val_1_u >= val_cpr){
+        rtn = val_1_u;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_2_l(){
+    let rtn = 100;
+    let val_2_l = Number($("#F2_l_y2").val());
+    let val_cpr = Number($("#F2_u_y2").val());
+
+    if (val_2_l <= val_cpr){
+        rtn = val_2_l;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_2_u(){
+    let rtn = 100;
+    let val_2_u = Number($("#F2_u_y2").val());
+    let val_cpr = Number($("#F2_l_y2").val());
+
+    if (val_2_u >= val_cpr){
+        rtn = val_2_u;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_3_l(){
+    let rtn = 100;
+    let val_3_l = Number($("#F2_l_y3").val());
+    let val_cpr = Number($("#F2_u_y3").val());
+
+    if (val_3_l <= val_cpr){
+        rtn = val_3_l;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_3_u(){
+    let rtn = 100;
+    let val_3_u = Number($("#F2_u_y3").val());
+    let val_cpr = Number($("#F2_l_y3").val());
+
+    if (val_3_u >= val_cpr){
+        rtn = val_3_u;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_4_l(){
+    let rtn = 100;
+    let val_4_l = Number($("#F2_l_y4").val());
+    let val_cpr = Number($("#F2_u_y4").val());
+
+    if (val_4_l <= val_cpr){
+        rtn = val_4_l;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_4_u(){
+    let rtn = 100;
+    let val_4_u = Number($("#F2_u_y4").val());
+    let val_cpr = Number($("#F2_l_y4").val());
+
+    if (val_4_u >= val_cpr){
+        rtn = val_4_u;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_5_l(){
+    let rtn = 100;
+    let val_5_l = Number($("#F2_l_y5").val());
+    let val_cpr = Number($("#F2_u_y5").val());
+
+    if (val_5_l <= val_cpr){
+        rtn = val_5_l;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_5_u(){
+    let rtn = 100;
+    let val_5_u = Number($("#F2_u_y5").val());
+    let val_cpr = Number($("#F2_l_y5").val());
+
+    if (val_5_u >= val_cpr){
+        rtn = val_5_u;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_6_l(){
+    let rtn = 100;
+    let val_6_l = Number($("#F2_l_y6").val());
+    let val_cpr = Number($("#F2_u_y6").val());
+
+    if (val_6_l <= val_cpr){
+        rtn = val_6_l;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_6_u(){
+    let rtn = 100;
+    let val_6_u = Number($("#F2_u_y6").val());
+    let val_cpr = Number($("#F2_l_y6").val());
+
+    if (val_6_u >= val_cpr){
+        rtn = val_6_u;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_7_l(){
+    let rtn = 100;
+    let val_7_l = Number($("#F2_l_y7").val());
+    let val_cpr = Number($("#F2_u_y7").val());
+
+    if (val_7_l <= val_cpr){
+        rtn = val_7_l;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_7_u(){
+    let rtn = 100;
+    let val_7_u = Number($("#F2_u_y7").val());
+    let val_cpr = Number($("#F2_l_y7").val());
+
+    if (val_7_u >= val_cpr){
+        rtn = val_7_u;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_8_l(){
+    let rtn = 100;
+    let val_8_l = Number($("#F2_l_y8").val());
+    let val_cpr = Number($("#F2_u_y8").val());
+
+    if (val_8_l <= val_cpr){
+        rtn = val_8_l;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_8_u(){
+    let rtn = 100;
+    let val_8_u = Number($("#F2_u_y8").val());
+    let val_cpr = Number($("#F2_l_y8").val());
+
+    if (val_8_u >= val_cpr){
+        rtn = val_8_u;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_9_l(){
+    let rtn = 100;
+    let val_9_l = Number($("#F2_l_y9").val());
+    let val_cpr = Number($("#F2_u_y9").val());
+
+    if (val_9_l <= val_cpr){
+        rtn = val_9_l;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
+
+function listener_9_u(){
+    let rtn = 100;
+    let val_9_u = Number($("#F2_u_y9").val());
+    let val_cpr = Number($("#F2_l_y9").val());
+
+    if (val_9_u >= val_cpr){
+        rtn = val_9_u;
+    }
+    else{
+        rtn = val_cpr;
+    }
+    return rtn;
+}
